84764e1b0ed5966daba9a175ec2a86c5c769e285
rm86480 <rm86480@BF-P4L1-M04.fieb.local>
2023-06-01 11:16:47 -0300

    Ediçao da mensagem Hello World!

--- src/main/java/com/explaintech/comercio_eletronico/ComercioEletronicoApplication.java
+++ src/main/java/com/explaintech/comercio_eletronico/ComercioEletronicoApplication.java
@@ -8,6 +8,8 @@ public class ComercioEletronicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComercioEletronicoApplication.class, args);
+	System.out.println("Hello world! Joao agora esta no GIT HUB");
 	}
+	
 
 }
