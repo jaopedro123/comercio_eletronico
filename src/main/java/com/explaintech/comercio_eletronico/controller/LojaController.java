2dfa31444ed9daa4ea2e9a382d5df4d915857d25
rm86480 <rm86480@BF-P4L4-M11.fieb.local>
2023-06-19 08:49:40 -0300

    DIA 19/06/2023

--- src/main/java/com/explaintech/comercio_eletronico/controller/LojaController.java
+++ src/main/java/com/explaintech/comercio_eletronico/controller/LojaController.java
@@ -1,6 +1,10 @@
 package com.explaintech.comercio_eletronico.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,23 +15,41 @@ import com.explaintech.comercio_eletronico.model.Produto;
 public class LojaController {
 	
 	@GetMapping("/listar")
-	public String testProduto() {
-		System.out.println("Lista de produtos");
-		
-		Produto produto = new Produto();
-		
-		produto.setNome("Televisor Samsung 70'");
-		produto.setCodigoDeBarras("1234567er2e2");
-		produto.setId("122");
-		produto.setPreco("1500.00");
+	public String testProduto(Model model) {
 		
 		
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
+		
+		
+		Produto p1 = new Produto();
+		
+		p1.setNome("Televisor Samsung 70'");
+		p1.setCodigoDeBarras("1234567er2e2");
+		p1.setId(122);
+		p1.setPreco(6500.00);
+		p1.setDescricao("Tv sansung 8k full hd");
+		
+		
+		Produto p2 = new Produto();
+		
+		p2.setNome("Lava roupa brastemp");
+		p2.setCodigoDeBarras("132j31j3321");
+		p2.setId(202);
+		p2.setPreco(4000.00);
+		p2.setDescricao("3 litros");
+		
+		
+		listaDeProdutos.add(p1);
+		listaDeProdutos.add(p2);
+		
+		model.addAttribute("listaDeProdutos", listaDeProdutos);
 		
 		return "Produtos"; // Retorna nome do template
 		
 	}
+}
 	
 
 
 
-}
+
--- src/main/java/com/explaintech/comercio_eletronico/model/Produto.java
+++ src/main/java/com/explaintech/comercio_eletronico/model/Produto.java
@@ -6,6 +6,8 @@ public class Produto {
 	private String nome;
 	private String codigoDeBarras;
 	private double preco;
+	private String descricao;
+	
 	
 	// public: Acesso liberado para todas as classes
 	
@@ -14,36 +16,36 @@ public class Produto {
 	// Metodos Setter's -> Ultilizados para "atribuir" valor ao atributo
 	// Metodos Getter's -> Ultilizado para "recuperar" o valor do atributo
 
-	public void setId(Long id) {
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
 		this.id = id;
 	}
-	
-	public Long getId() {
-		return id;	
-	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getCodigoDeBarras() {
 		return codigoDeBarras;
 	}
-
 	public void setCodigoDeBarras(String codigoDeBarras) {
 		this.codigoDeBarras = codigoDeBarras;
 	}
-
 	public double getPreco() {
 		return preco;
 	}
-
 	public void setPreco(double preco) {
 		this.preco = preco;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 	
 	
